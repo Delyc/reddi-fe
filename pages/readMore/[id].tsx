f5715f8af1a638f6fc25569d91414ec8f04a6c08
Delyc <d.twizeyima@alustudent.com>
2023-02-21 22:55:16 +0200

    Feat:Styling one post page

--- pages/readMore/[id].tsx
+++ pages/readMore/[id].tsx
@@ -8,6 +8,7 @@ import getUserToken from "utils/getUserToken";
 import Navbar from "@/components/layouts/Navbar";
 import Footer from "@/components/layouts/Footer";
 import PageWrapper from "@/components/wrappers/PageWrapper";
+import postImage from '../../public/assets/post3.webp'
 
 const ReadMore = () => {
     const [token, setToken] = useState("")
@@ -94,67 +95,72 @@ const ReadMore = () => {
     };
     return (
         <>
-        <PageWrapper >
+            <PageWrapper >
 
-        <Navbar />
-            <div className="more mt-20">
-                {post ? (
-
-
-                    <>
-
-                        <div>
-                            <p>{post.title}</p>
-                            <p></p>
-                            {/* <Image src={post.image} alt="imahe" width={100} height={100} /> */}
-                        </div>
+                <Navbar />
+                <section className="grid grid-cols-3 gap-10 py-20 mt-20">
+                    <div className="col-span-2 ">
+                        {post ? (
 
 
+                            <>
 
-                        <div>
-                            <form action="" >
+                                <div className="px-20 mx-auto flex flex-col gap-3">
+                                    <Image className="w-full h-[60vh] object-contain rounded-xl" src={postImage} alt="Post image" width={100} height={100} />
+                                    <p className="text-xl text-[#eb5b39] font-bold">{post.title}</p>
+                                    <p>{post.body}</p>
+                                    <p></p>
+                                    <div className="flex gap-4">
+
+                                        <button
+                                            onClick={(e) => upvote(e, post._id)}
+
+                                        > like ({post?.meta?.upvotes || 0})
+                                        </button>
+                                        <button
+                                            onClick={(e) => downvote(e, post._id)}
+
+                                        >
+                                            dislike ({post?.meta?.downvotes || 0})
+                                        </button>
+                                    </div>
+                                    {/* <Image src={post.image} alt="imahe" width={100} height={100} /> */}
+                                </div>
+
+
+
+
+
+
+
+                            </>
+                        ) : (
+                            <h1>Loading</h1>
+                        )}
+                    </div>
+                    <div className="post-com">
+                        <div className="flex flex-col gap-3">
+                            <h1>What's on your mind ?!</h1>
+                            <form action="" className="flex flex-col gap-3" >
                                 <textarea
-                                    className="form-control"
-                                    placeholder="Comment"
+                                    className="form-control border py-5 px-5 shadow rounded-lg outline-none"
+                                    placeholder="Leave your Comment here!"
                                     name="body"
+                                    rows={5}
                                     onChange={inputHandler}
                                 ></textarea>
-                                <button className="comment" type="submit" onClick={onSubmit}>comment</button>
+                                <button className="comment bg-[#eb5b39] text-white font-semibold rounded py-2 px-5 w-36" type="submit" onClick={onSubmit}>comment</button>
                             </form>
                         </div>
-                        <div className="flex gap-4">
-
-                            <button
-                                onClick={(e) => upvote(e, post._id)}
-
-                            > like ({post?.meta?.upvotes || 0})
-                            </button>
-                            <button
-                                onClick={(e) => downvote(e, post._id)}
-
-                            >
-                                dislike ({post?.meta?.downvotes || 0})
-                            </button>
-                            <p>Like</p>
-                            <p>dislike</p>
-                            <p>Comment</p>
-
-                        </div>
-
-                        <div className="post-com">
-                            <h4>commmets</h4>
-
+                        <div className="mt-10">
                             {comments.map((comment: any, index: number) => {
-                                return <div key={index} className="comuser"> <p>{comment.body} by <span>{comment.user.firstName}</span>  </p>
+                                return <div key={index} className="comuser"> <p>{comment.body} by <span className="text-[#eb5b39] font-bold">{comment.user.firstName}</span>  </p>
 
                                 </div>;
                             })}
                         </div>
-                    </>
-                ) : (
-                    <h1>Loading</h1>
-                )}
-            </div>
+                    </div>
+                </section>
             </PageWrapper>
             <Footer />
         </>
